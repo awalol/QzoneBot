2a0f790d6d4567fc2fd9032d94c0e3828bfb1526
CNawalol <awalol@aliyun.com>
2021-05-03 14:03:40 +0800

    1.增加在下次推歌的时候会自动重试之前推送之前发送失败的说说（为什么我不加到线程？我不会） 2.优化了空间登陆 3.修复了一些bug

--- src/main/kotlin/cn/awalol/qzoneBot/Main.kt
+++ src/main/kotlin/cn/awalol/qzoneBot/Main.kt
@@ -1,12 +1,12 @@
 package cn.awalol.qzoneBot
 
 import cn.awalol.qzoneBot.bean.qqMusic.Singer
+import cn.awalol.qzoneBot.bean.qqMusic.SongInfo
 import com.fasterxml.jackson.databind.ObjectMapper
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import net.mamoe.mirai.Bot
 import net.mamoe.mirai.BotFactory
-import net.mamoe.mirai.contact.User
 import net.mamoe.mirai.event.Event
 import net.mamoe.mirai.event.events.FriendMessageEvent
 import net.mamoe.mirai.event.events.MessageEvent
@@ -14,7 +14,6 @@ import net.mamoe.mirai.event.events.StrangerMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
 import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.Image.Key.queryUrl
-import net.mamoe.mirai.message.data.MessageChain
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.utils.BotConfiguration
 import kotlin.system.exitProcess
@@ -25,9 +24,12 @@ val template = "#推歌意向征集#\n" +
         "《%s》（%s）"
 val singerBlackList = listOf("0011jjK40orUJx","002nXp292LIOGV","0022eAG537I1bg")
 val client = HttpClient(CIO)
+lateinit var qqUin : String
+val waitToRepublish : HashMap<SongInfo,Image?> = HashMap()
 
 suspend fun main(args : Array<String>){
-    val bot = BotFactory.newBot(args[0].toLong(), args[1]) {
+    qqUin = args[0]
+    val bot = BotFactory.newBot(qqUin.toLong(), args[1]) {
         protocol = BotConfiguration.MiraiProtocol.ANDROID_PAD
         fileBasedDeviceInfo()
     }
@@ -46,68 +48,88 @@ suspend fun main(args : Array<String>){
     }
 
     bot.globalEventChannel().filter { event: Event -> event is FriendMessageEvent || event is StrangerMessageEvent }.subscribeAlways<MessageEvent> {
-        push(message,bot,sender)
+        var image : Image? = null
+        var content = ""
+        message.forEach { singleMessage ->
+            if(singleMessage is Image){
+                image = Image(singleMessage.imageId)
+                bot.logger.info(singleMessage.imageId)
+            }else if(singleMessage is PlainText){
+                content = singleMessage.content
+                bot.logger.info("PlainText " + singleMessage.content)
+            }
+        }
+
+        if(qzoneCookie.isNotEmpty() && content.isNotEmpty()) {
+            if("((?<=《).+(?=》))".toRegex().containsMatchIn(content) && "(?<=[（|(]).+(?=[）|)])".toRegex().containsMatchIn(content)){
+                //获取歌曲信息
+                val songName = "((?<=《).+(?=》))".toRegex().find(content)!!.value
+                val songSinger = "(?<=[（|(]).+(?=[）|)])".toRegex().find(content)!!.value
+                bot.logger.info("$songName $songSinger")
+                val songInfo = MusicApi.qqMusicSongInfo(
+                    MusicApi.qqMusicSearch(
+                        songName,
+                        songSinger
+                    )!!.songmid!!
+                )
+
+                try {
+                    push(songInfo,bot,image)
+                    //重试发送未发送成功的说说
+                    waitToRepublish.forEach { (t, u) ->
+                        println("尝试重新发送之前发送失败的说说")
+                        push(t, bot, u)
+                        Thread.sleep(10000)
+                    }
+                }catch (e : Exception){
+                    e.printStackTrace()
+                    if(!waitToRepublish.containsKey(songInfo)){
+                        waitToRepublish[songInfo] = image
+                    }
+                }
+            }
+        }else if(qzoneCookie.isEmpty()){
+            bot.logger.error("QQ空间未登录")
+            bot.getFriend(3512311532)!!.sendMessage("[Error] 说说发送失败，QQ空间未登录")
+        }
     }
 }
 
-suspend fun push(messageChain : MessageChain,bot : Bot,sender : User){
-    var image : Image? = null
-    lateinit var message : String
-    messageChain.forEach { singleMessage ->
-        if(singleMessage is Image){
-            image = Image(singleMessage.imageId)
-            bot.logger.info(singleMessage.imageId)
-        }else if(singleMessage is PlainText){
-            message = singleMessage.content
-            bot.logger.info("PlainText " + singleMessage.content)
+suspend fun push(songInfo : SongInfo, bot : Bot, image : Image?){
+    val songData = songInfo.data[0]
+
+    //黑名单歌手判断
+    songData.singer.forEach { singer: Singer ->
+        if (singerBlackList.contains(singer.mid)) {
+            bot.logger.error("黑名单歌手:" + singer.title)
+            return
         }
     }
 
-    if(qzoneCookie.isNotEmpty()) {
-        if("((?<=《).+(?=》))".toRegex().containsMatchIn(message) && "(?<=[（|(]).+(?=[）|)])".toRegex().containsMatchIn(message)){
-            //获取歌曲信息
-            val songName = "((?<=《).+(?=》))".toRegex().find(message)!!.value
-            val songSinger = "(?<=[（|(]).+(?=[）|)])".toRegex().find(message)!!.value
-            bot.logger.info("$songName $songSinger")
-            val songInfo = MusicApi.qqMusicSongInfo(
-                MusicApi.qqMusicSearch(
-                    songName,
-                    songSinger
-                )!!.songmid!!
-            )
-            val songData = songInfo.data[0]
-
-            //黑名单歌手判断
-            songData.singer.forEach { singer: Singer ->
-                if (singerBlackList.contains(singer.mid)) {
-                    bot.logger.error(sender.id.toString() + " 尝试推送黑名单歌手:" + singer.title)
-                    return
-                }
-            }
-
-            //发送说说
-            if (image != null) {
-                val test = QzoneUtil.publishShuoshuo(
-                    template.format(
-                        songData.title,
-                        MusicApi.getSingers(songData.singer)
-                    ),
-                    image!!.queryUrl()
-                )
-                println(test)
-            } else {
-                val test = QzoneUtil.publishShuoshuo(
-                    template.format(
-                        songData.title,
-                        MusicApi.getSingers(songData.singer)
-                    ),
-                    "http://y.gtimg.cn/music/photo_new/T002R800x800M000%s.jpg".format(songData.album.mid)
-                )
-                println(test)
-            }
+    //发送说说
+    if (image != null) {
+        val test = QzoneUtil.publishShuoshuo(
+            template.format(
+                songData.title,
+                MusicApi.getSingers(songData.singer)
+            ),
+            image!!.queryUrl()
+        )
+        println(test)
+        if(waitToRepublish.containsKey(songInfo)){
+            waitToRepublish.remove(songInfo)
         }
-    }else{
-        bot.logger.error("QQ空间未登录")
-        bot.getFriend(3512311532)!!.sendMessage("[Error] 说说发送失败，QQ空间未登录")
+    } else {
+        val test = QzoneUtil.publishShuoshuo(
+            template.format(
+                songData.title,
+                MusicApi.getSingers(songData.singer)
+            ),
+            "http://y.gtimg.cn/music/photo_new/T002R800x800M000%s.jpg".format(songData.album.mid)
+        )
+        println(test)
+        if(waitToRepublish.containsKey(songInfo)){
+            waitToRepublish.remove(songInfo)
+        }
     }
 }
--- src/main/kotlin/cn/awalol/qzoneBot/QzoneUtil.kt
+++ src/main/kotlin/cn/awalol/qzoneBot/QzoneUtil.kt
@@ -5,10 +5,22 @@ import cn.awalol.qzoneBot.bean.qzoneSuosuo.PicinfoX
 import cn.awalol.qzoneBot.bean.qzoneSuosuo.UploadPic
 import io.ktor.client.request.*
 import io.ktor.http.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
+import org.openqa.selenium.By
 import org.openqa.selenium.Dimension
 import org.openqa.selenium.chrome.ChromeDriver
+import org.openqa.selenium.remote.RemoteWebDriver
 import java.net.URLEncoder
 import java.util.*
+
+const val clickScript = "var faces = document.getElementsByClassName(\"face\");\n" +
+        "for(i = 0;i < faces.length;i++){\n" +
+        "    if(faces[i].getAttribute(\"uin\") == \"%s\"){\n" +
+        "        pt.qlogin.imgClick(faces[i])\n" +
+        "    }\n" +
+        "}"
 
 object QzoneUtil {
     private fun getGtk(sKey: String): Long {
@@ -20,22 +32,20 @@ object QzoneUtil {
     }
 
     fun login(){
-        val driver = ChromeDriver()
-        driver.manage().window().size = Dimension(305,420);
-        driver.get("https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https%3A//qzs.qq.com/qzone/v6/portal/proxy.html&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=22&target=self&s_url=https%3A%2F%2Fqzs.qzone.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&pt_qr_app=%E6%89%8B%E6%9C%BAQQ%E7%A9%BA%E9%97%B4&pt_no_auth=1")
-        try {
-            while (true){
-                if(driver.currentUrl.contains("https://user.qzone.qq.com/")){
-                    driver.manage().cookies.forEach { cookie ->
-                        if(cookie.name.contentEquals("p_uin").or(cookie.name.contentEquals("p_skey"))){
-                            qzoneCookie.put(if (cookie.name.contentEquals("p_uin")) "p_uin" else "p_skey",cookie.value)
-                        }
+        val provider = ChromeDriver()
+        provider.manage().window().size = Dimension(305,420)
+        provider.get("https://xui.ptlogin2.qq.com/cgi-bin/xlogin?proxy_url=https%3A//qzs.qq.com/qzone/v6/portal/proxy.html&daid=5&&hide_title_bar=1&low_login=0&qlogin_auto_login=1&no_verifyimg=1&link_target=blank&appid=549000912&style=22&target=self&s_url=https%3A%2F%2Fqzs.qzone.qq.com%2Fqzone%2Fv5%2Floginsucc.html%3Fpara%3Dizone&pt_qr_app=%E6%89%8B%E6%9C%BAQQ%E7%A9%BA%E9%97%B4&pt_no_auth=1")
+        provider.executeScript(clickScript.format(qqUin))
+        while (true){
+            if(provider.currentUrl.contains("https://user.qzone.qq.com/")){
+                provider.manage().cookies.forEach { cookie ->
+                    if(cookie.name.contentEquals("p_uin").or(cookie.name.contentEquals("p_skey"))){
+                        qzoneCookie.put(if (cookie.name.contentEquals("p_uin")) "p_uin" else "p_skey",cookie.value)
                     }
-                    return
                 }
+                provider.close()
+                return
             }
-        }finally {
-            driver.quit()
         }
     }
 
@@ -48,21 +58,25 @@ object QzoneUtil {
         val uploadPic1Response : String = client.post{
             url("https://mobile.qzone.qq.com/up/cgi-bin/upload/cgi_upload_pic_v2?g_tk=" + getGtk(qzoneCookie.getValue("p_skey")))
             headers{
-                append("cookie","p_uin=" + qzoneCookie.getValue("p_uin") + ";p_skey=" + qzoneCookie.getValue("p_skey") + ";")
+                append(HttpHeaders.Cookie,"p_uin=" + qzoneCookie.getValue("p_uin") + ";p_skey=" + qzoneCookie.getValue("p_skey") + ";")
                 append(HttpHeaders.ContentType,"application/x-www-form-urlencoded")
+                append(HttpHeaders.UserAgent,"Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36")
             }
-            body = "picture=$imageBase64&output_type=json&preupload=1&base64=1"
+            body = "picture=$imageBase64&output_type=json&preupload=1&base64=1&hd_quality=90"
         }
         println(getStringMiddleContent(uploadPic1Response,"_Callback(",");"))
         val uploadPic : UploadPic = objectMapper.readValue(getStringMiddleContent(uploadPic1Response,"_Callback(",");"),
             UploadPic::class.java) //JSON反序列化
 
+        Thread.sleep(1000)
+
         //upload Image to ablum
         val uploadPic2Response : String = client.post{
             url("https://mobile.qzone.qq.com/up/cgi-bin/upload/cgi_upload_pic_v2?g_tk=" + getGtk(qzoneCookie.getValue("p_skey")))
             headers{
-                append("cookie","p_uin=" + qzoneCookie.getValue("p_uin") + ";p_skey=" + qzoneCookie.getValue("p_skey") + ";")
+                append(HttpHeaders.Cookie,"p_uin=" + qzoneCookie.getValue("p_uin") + ";p_skey=" + qzoneCookie.getValue("p_skey") + ";")
                 append(HttpHeaders.ContentType,"application/x-www-form-urlencoded")
+                append(HttpHeaders.UserAgent,"Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36")
             }
             body = "output_type=json&preupload=2&md5=${uploadPic.filemd5}&filelen=${uploadPic.filelen}&refer=shuoshuo&albumtype=7"
         }
@@ -75,11 +89,12 @@ object QzoneUtil {
             url("https://mobile.qzone.qq.com/mood/publish_mood?g_tk=" + getGtk(qzoneCookie.getValue("p_skey")))
             headers {
                 append(
-                    "cookie",
+                    HttpHeaders.Cookie,
                     "p_uin=" + qzoneCookie.getValue("p_uin") + ";p_skey=" + qzoneCookie.getValue("p_skey") + ";"
                 )
+                append(HttpHeaders.UserAgent,"Mozilla/5.0 (Linux; Android 8.0.0; Pixel 2 XL Build/OPD1.170816.004) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Mobile Safari/537.36")
             }
-            body = "opr_type=publish_shuoshuo&content=$content&format=json&richval=" + (picInfo.albumid + "," + picInfo.sloc + "," + picInfo.lloc + ",," + picInfo.height + "," + picInfo.width + ",,,")
+            body = "opr_type=publish_shuoshuo&content=${URLEncoder.encode(content)}&format=json&richval=" + (picInfo.albumid + "," + picInfo.sloc + "," + picInfo.lloc + ",," + picInfo.height + "," + picInfo.width + ",,,")
         }
     }
 
