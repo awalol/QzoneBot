55267664bffeeda0bb5bb01356afd860f6a42e50
CNawalol <awalol@aliyun.com>
2021-10-02 22:32:10 +0800

    update dependencies - kotlin - mirai-core - ktor - junit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    kotlin("jvm") version "1.5.10"
+    kotlin("jvm") version "1.5.31"
     id("com.github.johnrengelman.shadow") version "6.1.0"
 }
 
@@ -12,17 +12,17 @@ repositories {
 }
 
 dependencies {
-    api("net.mamoe", "mirai-core", "2.7.0")
+    api("net.mamoe", "mirai-core", "2.8.0-M1")
     implementation("org.seleniumhq.selenium","selenium-java","4.0.0-beta-3")
-    implementation("io.ktor:ktor-client-core:1.5.1")
-    implementation("io.ktor:ktor-client-cio:1.5.1")
+    implementation("io.ktor:ktor-client-core:1.6.3")
+    implementation("io.ktor:ktor-client-cio:1.6.3")
     implementation("com.fasterxml.jackson.core","jackson-core","2.12.1")
     implementation("com.fasterxml.jackson.core","jackson-annotations","2.12.1")
     implementation("com.fasterxml.jackson.core","jackson-databind","2.12.1")
 
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.withType<Jar> {
